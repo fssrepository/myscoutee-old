7a747273c6af7ec20b40cd15f590ff1a897227a8
raxim <raxim.uk@gmail.com>
2023-06-04 17:50:28 +0200

    generated spring 3.0.1 / java 17

---
+++ server/projects/profile/src/main/java/com/raxim/myscoutee/MyscouteeApplication.java
@@ -0,0 +1,13 @@
+package com.raxim.myscoutee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyscouteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyscouteeApplication.class, args);
+	}
+
+}
---
+++ server/projects/profile/src/test/java/com/raxim/myscoutee/MyscouteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxim.myscoutee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyscouteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
