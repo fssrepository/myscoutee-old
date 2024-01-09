b26209d7786f836052a163706068311686e3abeb
fssrepository@gmail.com <fssrepository@gmail.com>
2024-01-10 00:02:22 +0100

    credentials skeleton

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,1 +1,19 @@
-TODO
+export const environment = {
+  production: true,
+  serverUrl: "http://localhost:8080/",
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  vapid: '',
+  groupTypes: {
+    d: 'dating',
+    b: 'business',
+    t: 'job',
+    a: 'admin',
+  }
+};
